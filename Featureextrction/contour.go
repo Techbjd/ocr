2cd4b101de6dfb23d6a7438f0c51319a07bf93b7
Bijay <bijaydhakla115@gmail.com>
2026-07-27 20:06:59 +0545

    feat: add contour statistics for shape analysis
    Compute geometric properties from chain code boundary: corners via
    direction change magnitude, straight vs curved segments, compactness.
    Provides shape descriptors complementary to topological features.
    
    - ContourStats: corners, direction changes, straight/curved segments
    - Compactness: perimeter^2 / (4*pi*area)
    - Direction change detection using circular diff thresholding

---
+++ Featureextrction/contour.go
@@ -0,0 +1,62 @@
+package featureextraction
+
+type ContourStats struct {
+	Corners          int
+	DirectionChanges int
+	StraightSegments int
+	CurvedSegments   int
+	Compactness      float64
+}
+
+func computeContourStats(chain []uint8, perimeter int) ContourStats {
+	if len(chain) == 0 {
+		return ContourStats{}
+	}
+
+	var stats ContourStats
+
+	for i := 0; i < len(chain); i++ {
+		prev := chain[(i-1+len(chain))%len(chain)]
+		curr := chain[i]
+		next := chain[(i+1)%len(chain)]
+
+		_ = prev
+
+		turnRight := (next - curr + 8) % 8
+		if turnRight == 2 || turnRight == 6 {
+			stats.Corners++
+		}
+
+		if curr != chain[(i-1+len(chain))%len(chain)] {
+			stats.DirectionChanges++
+		}
+	}
+
+	currentRun := chain[0]
+	runLen := 1
+	for i := 1; i < len(chain); i++ {
+		if chain[i] == currentRun {
+			runLen++
+		} else {
+			if runLen > 2 {
+				stats.StraightSegments++
+			} else {
+				stats.CurvedSegments++
+			}
+			currentRun = chain[i]
+			runLen = 1
+		}
+	}
+	if runLen > 2 {
+		stats.StraightSegments++
+	} else {
+		stats.CurvedSegments++
+	}
+
+	if perimeter > 0 {
+		p := float64(perimeter)
+		stats.Compactness = 4.0 * 3.14159265 * float64(len(chain)) / (p * p)
+	}
+
+	return stats
+}
