c3d66c850381048a0db873d1113898ef29f16fad
Bijay <bijaydhakla115@gmail.com>
2026-07-28 08:05:14 +0545

    feat: add JSON marshal/unmarshal and Save/Load to SignatureStore
    CharacterSignature now uses custom JSON with human-readable
    character field. SignatureStore gains Save() and LoadSignatures().

--- Classifier/signature.go
+++ Classifier/signature.go
@@ -1,20 +1,80 @@
 package classifier
 
-import featureextraction "github.com/Techbjd/ocr/Featureextrction"
+import (
+	"encoding/json"
+	"os"
+
+	featureextraction "github.com/Techbjd/ocr/Featureextrction"
+)
+
+type CharacterJSON struct {
+	Character   string  `json:"character"`
+	Holes       int     `json:"holes"`
+	Endpoints   int     `json:"endpoints"`
+	Junctions   int     `json:"junctions"`
+	Area        int     `json:"area"`
+	Width       int     `json:"width"`
+	Height      int     `json:"height"`
+	Density     float64 `json:"density"`
+	AspectRatio float64 `json:"aspect_ratio"`
+	Horizontal  []int   `json:"horizontal"`
+	Vertical    []int   `json:"vertical"`
+	ChainCode   []uint8 `json:"chain_code"`
+}
 
 type CharacterSignature struct {
-	Character  rune
-	Holes      int
-	Endpoints  int
-	Junctions  int
-	Area       int
-	Width      int
-	Height     int
-	Density    float64
+	Character   rune
+	Holes       int
+	Endpoints   int
+	Junctions   int
+	Area        int
+	Width       int
+	Height      int
+	Density     float64
 	AspectRatio float64
-	Horizontal []int
-	Vertical   []int
-	ChainCode  []uint8
+	Horizontal  []int
+	Vertical    []int
+	ChainCode   []uint8
+}
+
+func (s CharacterSignature) MarshalJSON() ([]byte, error) {
+	return json.Marshal(CharacterJSON{
+		Character:   string(s.Character),
+		Holes:       s.Holes,
+		Endpoints:   s.Endpoints,
+		Junctions:   s.Junctions,
+		Area:        s.Area,
+		Width:       s.Width,
+		Height:      s.Height,
+		Density:     s.Density,
+		AspectRatio: s.AspectRatio,
+		Horizontal:  s.Horizontal,
+		Vertical:    s.Vertical,
+		ChainCode:   s.ChainCode,
+	})
+}
+
+func (s *CharacterSignature) UnmarshalJSON(b []byte) error {
+	var cj CharacterJSON
+	if err := json.Unmarshal(b, &cj); err != nil {
+		return err
+	}
+	runes := []rune(cj.Character)
+	if len(runes) > 0 {
+		s.Character = runes[0]
+	}
+	s.Holes = cj.Holes
+	s.Endpoints = cj.Endpoints
+	s.Junctions = cj.Junctions
+	s.Area = cj.Area
+	s.Width = cj.Width
+	s.Height = cj.Height
+	s.Density = cj.Density
+	s.AspectRatio = cj.AspectRatio
+	s.Horizontal = cj.Horizontal
+	s.Vertical = cj.Vertical
+	s.ChainCode = cj.ChainCode
+	return nil
 }
 
 func FromFeatureVector(fv featureextraction.FeatureVector, char rune) CharacterSignature {
@@ -48,4 +108,24 @@ func (ss *SignatureStore) Add(sig CharacterSignature) {
 
 func (ss *SignatureStore) Classify(unknown CharacterSignature) (rune, float64) {
 	return CompareSignatures(unknown, ss.Entries)
+}
+
+func (ss *SignatureStore) Save(path string) error {
+	data, err := json.MarshalIndent(ss, "", "  ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(path, data, 0644)
+}
+
+func LoadSignatures(path string) (*SignatureStore, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var ss SignatureStore
+	if err := json.Unmarshal(data, &ss); err != nil {
+		return nil, err
+	}
+	return &ss, nil
 }
